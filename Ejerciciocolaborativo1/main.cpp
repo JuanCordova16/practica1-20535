1b7108c062d1ce3e76e896ecc63822afdc162276
JuanCordova16 <jecordova5@espe.edu.ec>
2025-04-25 10:48:00 -0500

    Rama juan

--- Ejerciciocolaborativo1/main.cpp
+++ Ejerciciocolaborativo1/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
 using namespace std;
-
+void restar ();
+void restar (){
+int n1, n2,n3;
+cout<< "Ingrese el numero 1 que va a restar"<<endl;
+cin>>n1;
+cout<< "Ingrese el numero 2 que va a restar"<<endl;
+cin>>n2;
+n3=n1-n2;
+cout<<"El resultado es: "<< n3;
+}
 int main()
 {
+    restar();
     return 0;
 }
